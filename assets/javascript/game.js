c7864496f39d406410592a25dc471083b1ed1c5e
Lindsey Duquette <duquette.lr@gmail.com>
2019-04-01 23:13:38 -0400

    guessed letters populate array

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+
+
+var character = ["mario", "yoshi", "peach", "bowser", "luigi", "kirby"];
+
+wrongGuess = 6;
+
+var cpChoice = character[Math.floor(Math.random() * character.length)];
+
+console.log(cpChoice);
